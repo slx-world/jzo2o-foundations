91fa6d0b59450b227ecbb76f0f9bfce5816c6648
Song <slx9920@outlook.com>
2024-09-13 17:33:47 +0800

    区域服务分页查询

---
+++ src/main/java/com/jzo2o/foundations/mapper/ServeTypeMapper.java
@@ -0,0 +1,15 @@
+package com.jzo2o.foundations.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jzo2o.foundations.model.domain.ServeType;
+
+/**
+ * <p>
+ * 服务类型表 Mapper 接口
+ * </p>
+ *
+ * @author itcast
+ * @since 2023-07-03
+ */
+public interface ServeTypeMapper extends BaseMapper<ServeType> {
+}
